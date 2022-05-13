6a1fffc12fa0522a7b05dffa8e6421d89fbf9b55
Ren-hongchen <1280656287@qq.com>
2022-05-13 12:23:30 +0800

    init

---
+++ src/main/java/com/saul/blockchainlearing/BlockchainLearingApplication.java
@@ -0,0 +1,13 @@
+package com.saul.blockchainlearing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainLearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainLearingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saul/blockchainlearing/BlockchainLearingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saul.blockchainlearing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainLearingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
